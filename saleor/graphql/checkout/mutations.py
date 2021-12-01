95c90376c292e47b9cd30c868cee06d03a155572
aminziadna <amin.ziadna@bankleumi.co.il>
2021-12-01 16:37:35 +0200

    Add Sales and Visa fix 3

--- saleor/graphql/checkout/mutations.py
+++ saleor/graphql/checkout/mutations.py
@@ -610,7 +610,7 @@ class CheckoutShippingAddressUpdate(BaseMutation, I18nMixin):
         with transaction.atomic():
             shipping_address.save()
             change_shipping_address_in_checkout(checkout, shipping_address)
-        recalculate_checkout_discount(checkout, lines, info.context.discounts)
+        # recalculate_checkout_discount(checkout, lines, info.context.discounts)
 
         info.context.plugins.checkout_updated(checkout)
         return CheckoutShippingAddressUpdate(checkout=checkout)
